7d34fb62762200f5dea6e663ca4dd5788e3baa7a
sdboy <jiaoyiguanggo@gmail.com>
2018-08-26 00:15:49 +0800

    添加注释

--- src/main/java/com/emarket/dao/CartMapper.java
+++ src/main/java/com/emarket/dao/CartMapper.java
@@ -3,7 +3,27 @@ package com.emarket.dao;
 import com.emarket.bean.Cart;
 
 public interface CartMapper {
-    int insert(Cart record);
+  /**
+   * <p>插入商品记录</p>
+   * @method insert
+   * @author jiaoguang
+   * @date 2018/8/26 0:06
+   * @param record 记录
+   * @return int
+   * @since V1.0.0
+   * @version V1.0.0
+   */
+  int insert(Cart record);
 
-    int insertSelective(Cart record);
+  /**
+   * <p>插入商品记录</p>
+   * @method insertSelective
+   * @author jiaoguang
+   * @date 2018/8/26 0:13
+   * @param record 记录
+   * @return int
+   * @since V1.0.0
+   * @version V1.0.0
+   */
+  int insertSelective(Cart record);
 }
